fb0f1fd8d1b37bd242a9fe6acd2d7eb9b325b3e1
fattiger <nobody0036@proton.me>
2025-02-06 00:50:05 +0800

    +bbr

---
+++ luci-app-bbrswitch/luasrc/controller/bbrswitch.lua
@@ -0,0 +1,24 @@
+module("luci.controller.bbrswitch", package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/bbrswitch") then
+		return
+	end
+	local page
+	page = entry({"admin", "network", "bbrswitch"}, cbi("bbrswitch"), _("BBRswitch"), 101)
+	page.i18n = "bbrswitch"
+	page.dependent = true
+	
+	entry({"admin", "network", "bbrswitch", "status"}, call("action_status"))
+end
+
+local function bbr_status()
+	return luci.sys.call("/etc/init.d/bbrswitch check_status bbr") == 0
+end
+
+function action_status()
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({
+		bbr_state = bbr_status()
+    })
+end
---
+++ luci-app-bbrswitch/luasrc/model/cbi/bbrswitch.lua
@@ -0,0 +1,19 @@
+local kernel_version = luci.sys.exec("echo -n $(uname -r)")
+
+m = Map("BBRswitch")
+m.title	= translate("BBRswitch Settings")
+m.description = translate("BBR Congestion Control Algorithm Switch")
+
+m:append(Template("bbrswitch/bbrswitch_status"))
+
+s = m:section(TypedSection, "bbrswitch", "")
+s.addremove = false
+s.anonymous = true
+
+if nixio.fs.access("/lib/modules/" .. kernel_version .. "/tcp_bbr.ko") then
+bbr_cca = s:option(Flag, "bbr_cca", translate("BBR CCA"))
+bbr_cca.default = 0
+bbr_cca.description = translate("Using BBR CCA can improve TCP network performance effectively")
+end
+
+return m
