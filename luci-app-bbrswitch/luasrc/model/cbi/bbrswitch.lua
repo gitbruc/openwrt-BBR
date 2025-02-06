910e69a56dbc874fee30623784cac74a384a5c61
fattiger <nobody0036@proton.me>
2025-02-06 22:55:59 +0800

    repair

--- luci-app-bbrswitch/luasrc/model/cbi/bbrswitch.lua
+++ luci-app-bbrswitch/luasrc/model/cbi/bbrswitch.lua
@@ -11,8 +11,9 @@ s.addremove = false
 s.anonymous = true
 
 if nixio.fs.access("/lib/modules/" .. kernel_version .. "/tcp_bbr.ko") then
+config_get bbr_cca "bbrswitch" "bbr_cca" "0"
 bbr_cca = s:option(Flag, "bbr_cca", translate("BBR CCA"))
-bbr_cca.default = 0
+bbr_cca.default = bbr_cca
 bbr_cca.description = translate("Using BBR CCA can improve TCP network performance effectively")
 end
 
