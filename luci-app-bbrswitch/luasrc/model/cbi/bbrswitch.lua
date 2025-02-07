59bc3325d0020a1a162aebbf913eddd44ce7a6e3
fattiger <nobody0036@proton.me>
2025-02-07 17:45:16 +0800

    repair

--- luci-app-bbrswitch/luasrc/model/cbi/bbrswitch.lua
+++ luci-app-bbrswitch/luasrc/model/cbi/bbrswitch.lua
@@ -14,24 +14,6 @@ if nixio.fs.access("/lib/modules/" .. kernel_version .. "/tcp_bbr.ko") then
     bbr_cca = s:option(Flag, "bbr_cca", translate("BBR CCA"))
     bbr_cca.default = 0
     bbr_cca.description = translate("Using BBR CCA can improve TCP network performance effectively")
-
-    function bbr_cca.write(self, section, value)
-        -- 调用父类的 write 方法保存配置
-        Flag.write(self, section, value)
-
-        -- 根据值启动或停止 BBR
-        if value == "1" then
-            -- 启动 BBR
-            luci.sys.call("/etc/init.d/bbrswitch start >/dev/null 2>&1 &")
-        else
-            -- 停止 BBR
-            luci.sys.call("/etc/init.d/bbrswitch stop >/dev/null 2>&1 &")
-        end
-
-        -- 刷新页面或返回当前页面
-        --luci.sys.call("uci commit bbr")
-        --luci.http.redirect(luci.dispatcher.build_url(luci.dispatcher.context.requestpath))  -- 刷新当前页面
-    end
 end
 
 return m
